0061bd575c7b1a6867e5e12486ff5f1db816be21
demenSama <marcosvincius1051@gmail.com>
2023-12-08 20:17:01 -0300

    finalizado

--- ProjetoDio2.js
+++ ProjetoDio2.js
@@ -1,6 +1,6 @@
 let vitorias = 0;
 let derrotas = 0;
-let rank = ["ferro","bronze","prata","ouro","diamante","lendario","imortal"]; 
+let rank = ["Ferro","Bronze","Prata","Ouro","Diamante","LENDARIO","IMORTAL!"]; 
 
 
 //sistema de repetição para as vitorias e vitorias esta abaixo.
@@ -17,33 +17,43 @@ while(vitorias<=152 && derrotas<152){
         derrotas=derrotas+1;
         break;
     }derrotas+=1;
-//---------------------------------------------------------// terminar if e else, e depois resolver problema das partidas
-            let saldo = valores(vitorias,derrotas);
+//---------------------------------------------------------// 
+let saldo = valores(vitorias,derrotas);
 
-            function valores (num1,num2){
-                return num1-num2; 
-            }
-            if(saldo<10){
-                console.log("rank de aventureiro é "+rank[0]+ " pois ele tem um saldo de " +saldo);
-            }else 
-                if(10<saldo && saldo<=20){
-                    console.log("rank de aventureiro é "+rank[1]+ " pois ele tem um saldo de " +saldo);
+function valores (num1,num2){
+    return num1-num2; 
+}
+//-----------------------------------------------------// partidas contador
+let partidas = saldo/2
+//------------------------------------------------------// if e elses
+if(saldo<10){
+    console.log("O seu heroi, na partida número "+partidas+" ficou no rank "+rank[0]+ " pois ele tem um saldo de " +saldo+ " vitorias");
+    console.log("--------------------------------");
+}else 
+    if(10<saldo && saldo<=20){
+        console.log("O seu heroi, na partida número "+partidas+" ficou no rank "+rank[1]+ " pois ele tem um saldo de " +saldo+ " vitorias");
+        console.log("--------------------------------");
+    }else 
+        if (saldo>20 && saldo<=50){
+            console.log("O seu heroi, na partida número "+partidas+" ficou no rank "+rank[2]+ " pois ele tem um saldo de " +saldo+ " vitorias");
+            console.log("--------------------------------");
+        }else
+            if(saldo>50 && saldo<=80){
+                console.log("O seu heroi, na partida número "+partidas+" ficou no rank "+rank[3]+ " pois ele tem um saldo de " +saldo+ " vitorias");
+                console.log("--------------------------------");
+            }else
+                if(saldo>80 && saldo<=90){
+                    console.log("O seu heroi, na partida número "+partidas+" ficou no rank "+rank[4]+ " pois ele tem um saldo de " +saldo+ " vitorias");
+                    console.log("--------------------------------");
                 }else 
-                    if (saldo>20 && saldo<=50){
-                        console.log("rank de aventureiro é "+rank[2]+ " pois ele tem um saldo de " +saldo);
+                    if(saldo>90 && saldo<=100){
+                        console.log("O seu heroi, na partida número "+partidas+" ficou no rank "+rank[5]+ " pois ele tem um saldo de " +saldo+ " vitorias");
+                        console.log("--------------------------------");
                     }else
-                        if(saldo>50 && saldo<=80){
-                            console.log("rank de aventureiro é "+rank[3]+ " pois ele tem um saldo de " +saldo);
-                        }else
-                            if(saldo>80 && saldo<=90){
-                                console.log("rank de aventureiro é "+rank[4]+ " pois ele tem um saldo de " +saldo);
-                            }else 
-                                if(saldo>90 && saldo<=100){
-                                    console.log("rank de aventureiro é "+rank[5]+ " pois ele tem um saldo de " +saldo);
-                                }else
-                                    if(saldo>100){
-                                        console.log("rank de aventureiro é "+rank[6]+ " pois ele tem um saldo de " +saldo);
-                                    }
+                        if(saldo>100){
+                            console.log("O seu heroi, na partida número "+partidas+" ficou no rank "+rank[6]+ " pois ele tem um saldo de " +saldo+ " vitorias");
+                            console.log("--------------------------------");
+                        }
 
                                     
     }      
