b4c474ffbbf44d37b206d8bc911209f0ed9306fc
demenSama <marcosvincius1051@gmail.com>
2023-12-07 21:37:21 -0300

    aprendendo sobre as funções

--- projeto2.js
+++ projeto2.js
@@ -1,1 +1,8 @@
-console.log("ola");
+let UserName = getFirstName("Marcos vinicius", " ")
+console.log(UserName)
+UserName = getFirstName("Ceila-DIas", "-");
+console.log(UserName)
+function getFirstName(name,splitChar){
+let firstName = name.split(splitChar)[0]
+return firstName;
+}
